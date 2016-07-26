14a32efe4acb27f06a939b623d1959841cee38ec
Viridiana <sbelloni18@gmail.com>
2016-07-26 19:57:48 +0200

    modified:   zweel.py

--- zweel.py
+++ zweel.py
@@ -6,36 +6,32 @@ epsilon = 23.45
 
 ### CARTA Z #################################
 grado_z = 180
-cx = 350
-cy = 350
-r0 = 320
-r1 = 300
-r2 = 200
-r4 = 310
-grado_z = 180 
+rz1 = 200
+rz2 = 300
+rz3 = 320
+dz1 = rz3 - rz2
+grado_z = 0 
 dwg = svgwrite.Drawing('zweel.svg', profile = 'tiny')
-w0 = svgwrite.shapes.Circle(center=(cx,cy), r=r0, stroke_width = 2, stroke = "black", fill = "white")
-w1 = svgwrite.shapes.Circle(center=(cx,cy), r=r1, stroke_width = 2, stroke = "black", fill = "white")
-w2 = svgwrite.shapes.Circle(center=(cx,cy), r=r2, stroke_width = 2, stroke = "black", fill = "white")
-dwg.add(w0)
-dwg.add(w1)
-dwg.add(w2)
+zodiac = dwg.add(dwg.g(id='zodiac', stroke='black',  stroke_width = 2, fill = "white"))
+zodiac.add(dwg.circle(center=(0,0), r=rz3  ))
+zodiac.add(dwg.circle(center=(0,0), r=rz2  ))
+zodiac.add(dwg.circle(center=(0,0), r=rz1  ))
 
-### gradi #############
-zlines = dwg.add(dwg.g(id='zlines', stroke='black',  stroke_width = 2))
+### segni #############
 for i in range(0, 360, 30):
-		zlines.add(dwg.line( start=((math.cos(math.radians(grado_z+i)) * r0) + cx, (math.sin(math.radians(grado_z+i)) * -r0) + cy), end=((math.cos(math.radians(grado_z+i)) * r2) + cx, (math.sin(math.radians(grado_z+i)) * -r2) + cy)))
+	zodiac.add(dwg.line( start=((math.cos(math.radians(i)) * rz1) , (math.sin(math.radians(i)) * rz1) ), end=((math.cos(math.radians(i)) * rz3) , (math.sin(math.radians(i)) * rz3) )))
+### decani #############
 for i in range(0, 360, 10):
-		zlines.add(dwg.line( start=((math.cos(math.radians(grado_z+i)) * r0) + cx, (math.sin(math.radians(grado_z+i)) * -r0) + cy), end=((math.cos(math.radians(grado_z+i)) * r1) + cx, (math.sin(math.radians(grado_z+i)) * -r1) + cy)))
-for i in range(0, 360, 2):
-		zlines.add(dwg.line( start=((math.cos(math.radians(grado_z+i)) * r4) + cx, (math.sin(math.radians(grado_z+i)) * -r4) + cy), end=((math.cos(math.radians(grado_z+i)) * r1) + cx, (math.sin(math.radians(grado_z+i)) * -r1) + cy)))
-
+	zodiac.add(dwg.line( start=((math.cos(math.radians(i)) * rz2) , (math.sin(math.radians(i)) * rz2) ), end=((math.cos(math.radians(i)) * rz3) , (math.sin(math.radians(i)) * rz3) )))
+#for i in range(0, 360, 2):
+#		zlines.add(dwg.line( start=((math.cos(math.radians(grado_z+i)) * r4) , (math.sin(math.radians(grado_z+i)) * r4) ), end=((math.cos(math.radians(grado_z+i)) * r1) , (math.sin(math.radians(grado_z+i)) * r1) )))
+#
 ### gradi #############
-signs = dwg.add(dwg.g(id='signs', stroke='black',  stroke_width = 2, fill = 'white'))
-grado_z = 195 
-for i in range(0, 360, 30):
-		signs.add(dwg.circle(center=((math.cos(math.radians(grado_z+i)) * r0) + cx, (math.sin(math.radians(grado_z+i)) * -r0) + cy), r=20))
-
+#signs = dwg.add(dwg.g(id='signs', stroke='black',  stroke_width = 2, fill = 'white'))
+#grado_z = 15 
+#for i in range(0, 30, 30):
+#		signs.add(dwg.circle(center=((math.cos(math.radians(grado_z+i)) * r0) , (math.sin(math.radians(grado_z+i)) * r0) ), r=20))
+#
 
 
 
